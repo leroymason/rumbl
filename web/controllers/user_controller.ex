3d5bd686fab246b4e8896c16e6cfa7ef533840fa
Leroy Mason <leroy@memeware.com>
2016-10-15 09:51:12 +1100

    programming phoenix book, chapter 6 completed

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,5 +1,6 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  plug :authenticate_user when action in [:index, :show]
   alias Rumbl.User
 
 
@@ -24,11 +25,14 @@ defmodule Rumbl.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+          |> Rumbl.Auth.login(user)
           |> put_flash(:info, "#{user.name} created!")
           |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
   end
+
+
 
 end
